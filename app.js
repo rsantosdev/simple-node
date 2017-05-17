c2b1acbe3309a8d45b911b576ac29ffe752e813f
Rafael dos Santos <p.4ever@gmail.com>
2017-05-17 10:57:11 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require('http'); //add the http module
+
+//Create a server
+var myServer = http.createServer(function (request, response) {
+  // Return something from server
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello Node\n");
+}); //create a server
+
+//Bind server to a port
+myServer.listen(3000);
